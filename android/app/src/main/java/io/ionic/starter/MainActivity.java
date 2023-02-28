cdb46958ba90fa146f12cc4aad57796a0266c956
Closus <claudio@manca.be>
2023-02-28 10:24:27 +0100

    [FIX] Java main activity

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -6,16 +6,11 @@ import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
-import com.dutchconcepts.capacitor.barcodescanner.BarcodeScanner;
 
 
 public class MainActivity extends BridgeActivity {
   @override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      add(BarcodeScanner.class);
-    }});
   }
 }
