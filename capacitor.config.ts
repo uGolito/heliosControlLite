1fd0e353ba946c13c3cf7bb1655547a67eb1835f
Closus <claudio@manca.be>
2023-03-20 14:45:07 +0100

    [FIX] Change name in capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'helios.mobile.light',
   appName: 'Mobile',
   webDir: 'www',
   bundledWebRuntime: false
