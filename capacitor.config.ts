4bb530a020b7d6ed48d2d78438aa8282437025fe
Closus <claudio@manca.be>
2023-04-15 17:49:34 +0200

    [TEST] changement name capacitor.config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'helios.control.light',
-  appName: 'Mobile',
+  appName: 'Helios Control Light',
   webDir: 'www',
   bundledWebRuntime: false
 };
